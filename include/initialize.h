ade39f1c098452765b39766f4da706d9adac4ab4
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-07-28 18:14:28 -0300

    att initialize header

--- include/initialize.h
+++ include/initialize.h
@@ -1,6 +1,9 @@
 #include "libs.h"
-
+#ifndef INITIALIZE_H
+#define INITIALIZE_H
 // =============== FUNÇÕES DE INICIALIZAÇÃO ===============
 SimulacaoAeroporto* inicializar_simulacao(int pistas, int portoes, int torres, int tempo_simulacao, int max_avioes);
 void inicializar_recursos(RecursosAeroporto* recursos, int pistas, int portoes, int torres);
 void inicializar_metricas(MetricasSimulacao* metricas);
+
+#endif
