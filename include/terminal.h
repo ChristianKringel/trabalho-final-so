0c3e7443929ad698411d9b439d85e2c0b2210550
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-07-28 18:17:52 -0300

    att

--- include/terminal.h
+++ include/terminal.h
@@ -9,6 +9,5 @@ void close_terminal_ncurses();
 void log_evento_ui(SimulacaoAeroporto* sim, Aviao* aviao, const char* formato, ...);
 void update_terminal_display(SimulacaoAeroporto* sim);
 const char* estado_para_str(EstadoAviao estado);
-void init_windows();
 
 #endif
