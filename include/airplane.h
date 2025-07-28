3df49fa62977b57ef289a866f0810be2d60776bd
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-07-28 18:23:37 -0300

    att

--- include/airplane.h
+++ include/airplane.h
@@ -7,7 +7,7 @@
 
 #ifndef AIRPLANE_H
 #define AIRPLANE_H
-// =============== FUNÇÕES ESPECÍFICAS POR TIPO DE VOO ===============
+
 int pouso_internacional(Aviao* aviao, SimulacaoAeroporto* sim);
 int pouso_domestico(Aviao* aviao, SimulacaoAeroporto* sim);
 int desembarque_internacional(Aviao* aviao, SimulacaoAeroporto* sim);
@@ -15,7 +15,6 @@ int desembarque_domestico(Aviao* aviao, SimulacaoAeroporto* sim);
 int decolagem_internacional(Aviao* aviao, SimulacaoAeroporto* sim);
 int decolagem_domestico(Aviao* aviao, SimulacaoAeroporto* sim);
 
-// =============== FUNÇÕES DE CRIAÇÃO DE AVIÕES ===============
 Aviao* criar_aviao(int id, TipoVoo tipo);
 void* criador_avioes(void* arg);
 TipoVoo gerar_tipo_voo_aleatorio();
