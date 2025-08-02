c1ea6ed2343ac63cc639d129ede4d4af4d490945
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-08-02 14:39:46 -0300

    att metrics header

--- include/metrics.h
+++ include/metrics.h
@@ -1,12 +1,15 @@
 #include "libs.h"
 #ifndef METRICS_H
 #define METRICS_H
-// =============== FUNÇÕES DE MÉTRICAS ===============
+
 void incrementar_metrica_pouso(MetricasSimulacao* metricas);
 void incrementar_metrica_desembarque(MetricasSimulacao* metricas);
 void incrementar_metrica_decolagem(MetricasSimulacao* metricas);
 void incrementar_aviao_sucesso(MetricasSimulacao* metricas);
 void incrementar_aviao_starvation(MetricasSimulacao* metricas);
 void incrementar_aviao_deadlock(MetricasSimulacao* metricas);
+void incrementar_contador_avioes_criados(MetricasSimulacao* metricas);
+void incrementar_voos_domesticos(MetricasSimulacao* metricas);
+void incrementar_voos_internacionais(MetricasSimulacao* metricas);
 
 #endif
