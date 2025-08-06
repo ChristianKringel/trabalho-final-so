e8cb9b1bdfedd3e2506644ba25efe3b49ffd03bd
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-08-05 22:09:11 -0300

    added logger.c

---
+++ src/logger.c
@@ -0,0 +1,29 @@
+#include "logger.h"
+
+static void format_file_timestamp(char* timestamp, size_t size) {
+    time_t agora = time(NULL);
+    struct tm* tm_info = localtime(&agora);
+    if (tm_info) {
+        strftime(timestamp, sizeof(timestamp), "%d/%m/%Y %H:%M:%S", tm_info);
+    } else {
+        strcpy(timestamp, "UNKNOWN");
+    }
+}
+
+void write_to_log_file(SimulacaoAeroporto* sim, Aviao* aviao, const char* buffer) {
+    FILE* arquivo_log = fopen("simulacao_log.txt", "a");
+
+    if (!arquivo_log) return;
+
+    char timestamp[64];
+    format_file_timestamp(timestamp, sizeof(timestamp));
+    
+    if (aviao && aviao->id > 0) {
+        char tipo_char = (aviao->tipo == VOO_DOMESTICO) ? 'D' : 'I';
+        fprintf(arquivo_log, "[%s] %c%02d  %s\n", timestamp, tipo_char, aviao->id, buffer);
+    } else {
+        fprintf(arquivo_log, "[%s] [SYSTEM]  %s\n", timestamp, buffer);
+    }
+    
+    fclose(arquivo_log);
+}
