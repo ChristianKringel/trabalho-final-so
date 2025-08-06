36bb20ff64332e53e287bc73b97e79af3f87d513
ViniHallmann <Viniciushallmann2015@gmail.com>
2025-08-05 22:09:01 -0300

    upt terminal header

--- include/terminal.h
+++ include/terminal.h
@@ -1,5 +1,6 @@
 #include "libs.h"
 #include "utils.h"
+#include "logger.h"
 
 #ifndef TERMINAL_H
 #define TERMINAL_H
@@ -13,8 +14,19 @@ static void draw_fids_panel(SimulacaoAeroporto* sim, int voos_ativos);
 const char* estado_para_str(EstadoAviao estado);
 void init_terminal_ncurses();
 void close_terminal_ncurses();
-void log_evento_ui(SimulacaoAeroporto* sim, Aviao* aviao, int cor, const char* formato, ...);
+
 void update_terminal_display(SimulacaoAeroporto* sim);
+
+void log_evento_ui(SimulacaoAeroporto* sim, Aviao* aviao, int cor, const char* formato, ...);
+static bool validate_log_params(SimulacaoAeroporto* sim, const char* formato);
+static void format_log_message(char* buffer, size_t size, const char* formato, va_list args);
+static void display_log_in_window(SimulacaoAeroporto* sim, Aviao* aviao, const char* buffer, int cor);
+static int draw_timestamp(WINDOW* win, int y, SimulacaoAeroporto* sim);
+static int draw_flight_prefix(WINDOW* win, int y, int x, Aviao* aviao);
+static int draw_system_prefix(WINDOW* win, int y, int x);
+static void draw_message_with_color(WINDOW* win, int y, int x, const char* buffer, int cor);
+static void finalize_log_display(WINDOW* win);
+
 
 #define PAIR_DEFAULT 1
 #define PAIR_HEADER  2
@@ -37,5 +49,12 @@ void update_terminal_display(SimulacaoAeroporto* sim);
 #define LOG_RESOURCE  PAIR_RESOURCE  
 #define LOG_TIMING    PAIR_TIMING    
 #define LOG_DEBUG     PAIR_DEBUG 
+
+#define HEADER_HEIGHT 1
+#define AIRSPACE_HEIGHT 3
+#define STATUS_WIDTH 28
+#define LOG_WIDTH 96
+#define FIDS_WIDTH (COLS - STATUS_WIDTH - LOG_WIDTH)
+#define MAIN_HEIGHT (LINES - HEADER_HEIGHT - AIRSPACE_HEIGHT)
 
 #endif
