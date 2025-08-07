57cdfb67dacd76d72486941c795fdcda522c57f6
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-08-07 14:01:27 -0300

    added status panel header

---
+++ include/status_panel.h
@@ -0,0 +1,10 @@
+#ifndef STATUS_PANEL_H
+#define STATUS_PANEL_H
+
+#include "libs.h"
+#include "utils.h"
+
+void manage_status_panel(SimulacaoAeroporto* sim, int voos_ativos, WINDOW* status_win);
+
+
+#endif
