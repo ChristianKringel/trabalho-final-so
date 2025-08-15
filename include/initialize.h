bc2e2d1ea1c37fdbe08342a3288d449294baaee9
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-08-15 15:19:57 -0300

    chore initialize.c

--- include/initialize.h
+++ include/initialize.h
@@ -1,12 +1,14 @@
 #include "libs.h"
 #ifndef INITIALIZE_H
 #define INITIALIZE_H
-// =============== FUNÇÕES DE INICIALIZAÇÃO ===============
+
+
 SimulacaoAeroporto* inicializar_simulacao(int pistas, int portoes, int torres, int tempo_simulacao, int max_avioes);
-void inicializar_recursos(RecursosAeroporto* recursos, int pistas, int portoes, int torres);
 void inicializar_metricas(MetricasSimulacao* metricas);
-void inicializar_banqueiro(Banqueiro* banco, int pistas, int portoes, int torres);
+void inicializar_banqueiro(RecursosAeroporto* recursos, int pistas, int portoes, int torres);
 void inicializar_pistas(RecursosAeroporto* recursos, int pistas);
 void inicializar_portoes(RecursosAeroporto* recursos, int portoes);
 void inicializar_torre(RecursosAeroporto* recursos, int capacidade);
+void inicializar_recursos(RecursosAeroporto* recursos, int pistas, int portoes, int torres);
+
 #endif
--- src/initialize.c
+++ src/initialize.c
@@ -1,59 +1,40 @@
 #include "initialize.h"
 #include "utils.h"
 
-void inicializar_recursos(RecursosAeroporto* recursos, int pistas, int portoes, int torres){
-    if (recursos == NULL) {
-        return; 
-    }
-
-    inicializar_pistas(recursos, pistas);
-    inicializar_portoes(recursos, portoes);
-    inicializar_torre(recursos, torres);
-    
-    // =============== INICIALIZAÇÃO DO BANQUEIRO ===============
-    inicializar_banqueiro(&recursos->banco, pistas, portoes, torres);
-    pthread_mutex_init(&recursos->mutex_banco, NULL);
-    pthread_cond_init(&recursos->cond_banco, NULL);
-}
-
 void inicializar_metricas(MetricasSimulacao* metricas){
     if (metricas == NULL) {
         return; 
     }
 
-    metricas->total_avioes_criados = 0;
-    metricas->avioes_finalizados_sucesso = 0;
-    metricas->avioes_falha_starvation = 0;
-    metricas->avioes_falha_deadlock = 0;
-    metricas->voos_domesticos_total = 0;
-    metricas->voos_internacionais_total = 0;
-    metricas->operacoes_pouso = 0;
-    metricas->operacoes_desembarque = 0;
-    metricas->operacoes_decolagem = 0;
+    metricas->total_avioes_criados          = 0;
+    metricas->avioes_finalizados_sucesso    = 0;
+    metricas->avioes_falha_starvation       = 0;
+    metricas->avioes_falha_deadlock         = 0;
+    metricas->voos_domesticos_total         = 0;
+    metricas->voos_internacionais_total     = 0;
+    metricas->operacoes_pouso               = 0;
+    metricas->operacoes_desembarque         = 0;
+    metricas->operacoes_decolagem           = 0;
 
     pthread_mutex_init(&metricas->mutex_metricas, NULL);
 }
 
-void inicializar_banqueiro(Banqueiro* banco, int pistas, int portoes, int torres) {
-    if (banco == NULL) {
-        return; 
-    }
-
+void inicializar_banqueiro(RecursosAeroporto* recursos, int pistas, int portoes, int torres) {
     for (int i = 0; i < MAX_AVIOES; i++) {
         for (int j = 0; j < N_RESOURCES; j++) {
-            banco->alocacao[i][j] = 0;
-            banco->necessidade[i][j] = 0;
+            recursos->banco.alocacao[i][j] = 0;
+            recursos->banco.necessidade[i][j] = 0;
         }
     }
 
     for (int j = 0; j < N_RESOURCES; j++) {
-        banco->disponivel[j] = 0;
+        recursos->banco.disponivel[j] = 0;
     }
-    banco->disponivel[RECURSO_PISTA] = 3;
-    banco->disponivel[RECURSO_PORTAO] = 5;
-    banco->disponivel[RECURSO_TORRE] = 2;
-
-    //pthread_mutex_init(&banco->mutex_banqueiro, NULL);
+    recursos->banco.disponivel[RECURSO_PISTA]  = pistas;
+    recursos->banco.disponivel[RECURSO_PORTAO] = portoes;
+    recursos->banco.disponivel[RECURSO_TORRE]  = 2;
+    pthread_mutex_init(&recursos->mutex_banco, NULL);
+    pthread_cond_init(&recursos->cond_banco, NULL);
 }
 
 void inicializar_pistas(RecursosAeroporto* recursos, int pistas){
@@ -76,7 +57,6 @@ void inicializar_portoes(RecursosAeroporto* recursos, int portoes){
     for (int i = 0; i < portoes; i++) {
         recursos->portao_ocupado_por[i] = -1; 
     }
-    
     pthread_mutex_init(&recursos->mutex_portoes, NULL);
     pthread_cond_init(&recursos->cond_portoes, NULL);
     inicializar_fila_prioridade(&recursos->fila_portoes);
@@ -95,13 +75,17 @@ void inicializar_torre(RecursosAeroporto* recursos, int capacidade){
     inicializar_fila_prioridade(&recursos->fila_torres);
 }
 
-
+void inicializar_recursos(RecursosAeroporto* recursos, int pistas, int portoes, int torres){
+    if (recursos == NULL) { return; }
+    inicializar_pistas(recursos, pistas);
+    inicializar_portoes(recursos, portoes);
+    inicializar_torre(recursos, torres);
+    inicializar_banqueiro(recursos, pistas, portoes, torres);
+}
 
 SimulacaoAeroporto* inicializar_simulacao(int pistas, int portoes, int torres, int tempo_simulacao, int max_avioes){
     SimulacaoAeroporto* sim = (SimulacaoAeroporto*)malloc(sizeof(SimulacaoAeroporto));
-    if (sim == NULL) {
-        return NULL; 
-    }
+    if (sim == NULL) { return NULL; }
     
     sim->max_avioes = max_avioes; 
     sim->avioes = (Aviao*)malloc(sim->max_avioes * sizeof(Aviao));
@@ -126,8 +110,6 @@ SimulacaoAeroporto* inicializar_simulacao(int pistas, int portoes, int torres, i
     sim->tempo_inicio = time(NULL);
     sim->tempo_pausado_total = 0;
     sim->inicio_pausa = 0;
-
-    
     sim->pausado = false;
     pthread_mutex_init(&sim->mutex_pausado, NULL);
     pthread_cond_init(&sim->cond_pausado, NULL);
